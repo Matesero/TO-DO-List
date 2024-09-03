d787e843e74f1cc0c91ceb6eca76c0316279b338
Matesero <grishenkov05@mail.ru>
2024-09-03 10:21:00 +0700

    add markup

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const editor = document.getElementById('editor');
+const downloadBtn = document.getElementById('download');
+const uploadBtn = document.getElementById('upload');
+const resetBtn = document.getElementById('reset');

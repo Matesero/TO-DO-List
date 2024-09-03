0ce5fac3e71f3ee0ffefe23f6c16b3f6fa6dd604
Matesero <grishenkov05@mail.ru>
2024-09-03 10:38:26 +0700

    add to-do, switch button

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ const editor = document.getElementById('editor');
 const downloadBtn = document.getElementById('download');
 const uploadBtn = document.getElementById('upload');
 const resetBtn = document.getElementById('reset');
+
+const switchs = document.getElementsByClassName('switch');
+Array.from(switchs).forEach(sw => {
+    sw.addEventListener('click', () => {
+       sw.classList.toggle('switch-on');
+       sw.parentNode.parentNode.parentNode.classList.toggle('complete');
+    });
+})
